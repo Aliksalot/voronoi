10ddad7397f4fbe94786b95630a6a43828fa7910
Alex <117526291+Aliksalot@users.noreply.github.com>
2023-07-31 12:03:02 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,63 @@
+
+const canvasSize = {w: 100, h: 100}
+
+const dotCount = 10
+let dots = []
+
+const p = []
+
+const getRandomColor = () => {
+	return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
+const createDots = () => {
+	for(let i = 0; i < dotCount; i ++){
+		dots.push({x: Math.random() * 800, y: Math.random() * 600, color: getRandomColor()});
+	}
+}
+
+const getClosestDotColor = (pixel) => {
+	
+	let dist = 999999999;
+	let closest = dots[0];
+	for(let i = 0; i < dotCount; i++){
+		const dot = dots[i]
+		const var_dist = (dot.x - pixel.x)**2 + (dot.y - pixel.y)**2
+		if(var_dist < dist){
+				dist = var_dist
+				closest = dot;
+		}
+	}
+	return closest.color;
+	
+}
+
+function init() {
+	createDots()
+	for(let i = 0; i < canvasSize.w; i++){
+		p.push([])
+		for(let j = 0; j < canvasSize.h; j ++){
+			const color = getClosestDotColor({x: i, y: j});
+			console.log(`x: ${i}, y: ${j}, color: ${color}`)
+			p[i].push({color: color})
+		}
+	}
+	
+}
+
+function draw() {
+    for(let i = 0; i < canvasSize.w; i++){
+		for(let j = 0; j < canvasSize.h; j++){
+			console.log(`drawing: x = ${i}, y = ${j}`)
+			context.fillStyle = p[i][j].color;
+			context.fillRect(i,j,1,1);
+		}
+	}
+}
+
+function update() {
+	
+}
+
+
+init();
